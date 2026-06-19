9efc04a721f346ac8f78b04c819494da14fce9a1
Elena Darevskaya <darevskaya.lena@gmail.com>
2026-06-19 16:12:54 +0300

    Update config and add lint

--- bin/server.ts
+++ bin/server.ts
@@ -11,17 +11,19 @@ server.on('error', (error: NodeJS.ErrnoException) => {
   if (error.syscall !== 'listen') throw error;
   switch (error.code) {
     case 'EACCES':
-      console.error(`Port ${port} requires elevated privileges`);
+      console.error(`Port ${String(port)} requires elevated privileges`);
       process.exit(1);
+      break;
     case 'EADDRINUSE':
-      console.error(`Port ${port} is already in use`);
+      console.error(`Port ${String(port)} is already in use`);
       process.exit(1);
+      break;
     default:
       throw error;
   }
 });
 server.on('listening', () => {
   const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${String(addr?.port)}`;
   console.log(`Listening on ${bind}`);
 });
